aa731267e65dd5b2b4e0a1ded249ad2d5d247da9
Наталья Козлова <natalakozlova@MacBook-Pro-Natala.local>
2026-02-10 20:09:38 +0300

    fixes first function

--- main.py
+++ main.py
@@ -2,3 +2,14 @@ from src.masks import get_mask_card_number , get_mask_account
 
 print(get_mask_card_number("7000792289606361")) # >> 7000 79** ****6361
 print(get_mask_account("73654108430135874305")) # >> **4305
+
+from src.widget import mask_account_card
+
+print(mask_account_card("Maestro 1596837868705199"))
+print(mask_account_card("Счет 64686473678894779589"))
+print(mask_account_card("MasterCard 7158300734726758"))
+print(mask_account_card("Счет 35383033474447895560"))
+print(mask_account_card("Visa Classic 6831982476737658"))
+print(mask_account_card("Visa Platinum 8990922113665229"))
+print(mask_account_card("Visa Gold 5999414228426353"))
+print(mask_account_card("Счет 73654108430135874305"))
---
+++ src/widget.py
@@ -0,0 +1,23 @@
+from src.masks import get_mask_account, get_mask_card_number
+
+def mask_account_card(card_or_account: str)-> str:
+    """
+    Функция принимает строку с типом и номером карты или счета
+    и возвращает строку с замаскированным номером
+    """
+
+    # номер в строке всегда последний
+    number = card_or_account.split() [-1]
+    # т.к  между названием и номером есть пробел, то +1
+    # с конца считаем количество символов номера и дальше текст
+    name = card_or_account[:-(len(number) +1)]
+
+    if "Счет" in card_or_account:
+        masked_number = get_mask_account(number)
+    else:
+        masked_number = get_mask_card_number(number)
+
+    return f"{name} {masked_number}"
+
+
+
