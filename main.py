5d03276309c7f89ad51819a8d389c3bb308754c5
Наталья Козлова <natalakozlova@MacBook-Pro-Natala.local>
2026-02-10 20:21:02 +0300

    fixes second f

--- main.py
+++ main.py
@@ -13,3 +13,7 @@ print(mask_account_card("Visa Classic 6831982476737658"))
 print(mask_account_card("Visa Platinum 8990922113665229"))
 print(mask_account_card("Visa Gold 5999414228426353"))
 print(mask_account_card("Счет 73654108430135874305"))
+
+from src.widget import get_date
+
+print(get_date("2024-03-11T02:26:18.671407"))
--- src/widget.py
+++ src/widget.py
@@ -8,7 +8,7 @@ def mask_account_card(card_or_account: str)-> str:
 
     # номер в строке всегда последний
     number = card_or_account.split() [-1]
-    # т.к  между названием и номером есть пробел, то +1
+    # т.к. между названием и номером есть пробел, то +1
     # с конца считаем количество символов номера и дальше текст
     name = card_or_account[:-(len(number) +1)]
 
@@ -18,6 +18,23 @@ def mask_account_card(card_or_account: str)-> str:
         masked_number = get_mask_card_number(number)
 
     return f"{name} {masked_number}"
+
+def get_date(date_str:str)->str:
+    """
+    Функция, которая принимает на вход строку
+    с датой в одном формате
+    и возвращает в другом формате
+    """
+
+    # берем первые 10 символов
+    date_part = date_str[:10]
+
+    # разделяем на год, месяц, день
+    year, month, day = date_part.split("-")
+
+    # собираем в нужном формате
+    return f"{day}.{month}.{year}"
+
 
 
 
