d0012198c1c95e02fb339317b456aa337a6f82d7
natalakozlova <natamedoed22@gmail.com>
2026-02-23 22:42:36 +0300

    fixes tests01

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+
+@pytest.fixture
+def card_16():
+    return "1234567812345678"
+
+
+@pytest.fixture
+def account_20():
+    return "12345678901234567890"
+
+
+
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,30 +1,49 @@
 import pytest
-from src.masks import get_mask_card_number
 
+from src.masks import get_mask_account, get_mask_card_number
 
-@pytest.fixture
-def valid_masked_card():
-    return "1234 56** **** 5678"
 
 
 @pytest.mark.parametrize(
-    "input_value, expected",
+    "card_value, expected",
     [
         # Стандартный 16-значный номер
         ("1234567812345678", "1234 56** **** 5678"),
         # С пробелами
         ("1234 5678 1234 5678", "1234 56** **** 5678"),
-        # С дефисами
-        ("1234-5678-1234-5678", "1234 56** **** 5678"),
         # Короткий номер (8 цифр)
-        ("12345678", "1234 56** 5678"),
+        ("12345678", ""),
         # Нестандартная длина (длинный номер)
-        ("12345678123456781234", "1234 56** **** 1234"),
+        ("12345678123456781234", ""),
         # Пустая строка
         ("", ""),
         # Строка без цифр
         ("Нет номера карты", ""),
     ],
 )
-def test_get_mask_card_number(input_value, expected):
-    assert get_mask_card_number(input_value) == expected
+def test_get_mask_card_number(card_value, expected):
+    assert get_mask_card_number(card_value) == expected
+
+
+
+@pytest.mark.parametrize(
+    "account_value, expected",
+    [
+        # Стандартный 20-значный номер
+        ("12345678901234567890", "**7890"),
+        # С пробелами
+        ("12345 678 9 0123 4567890", "**7890"),
+        # Короткий номер 5 цифр
+        ("12345", ""),
+        # Длинный номер
+        ("1234567890123456789012345", ""),
+        # Пустая строка
+        ("", ""),
+        # Строка без цифр
+        ("Нет номера счета", ""),
+    ],
+)
+def test_get_mask_account(account_value, expected):
+    assert get_mask_account(account_value) == expected
+
+
