cdc940fff9faf549425f19d1f49aa62c4597bb42
Natalia Kozlova <natamedoed22@gmail.com>
2026-02-27 15:38:44 +0300

    Revert "Feature/homework 10 02 tests"

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,6 +1,5 @@
 import pytest
-
-from src.widget import get_date, mask_account_card
+from src.widget import mask_account_card, get_date
 
 
 @pytest.mark.parametrize(
@@ -23,8 +22,8 @@ def test_mask_account_card(mask_account_card_value, expected):
         ("2024-03-11T02:26:18.671407", "11.03.2024"),
         ("2024-03-11", "11.03.2024"),
         ("2024-0a-11", ""),  # не цифры
-        ("24-03-11", ""),  # год не 4 цифры
-        ("2024-3-11", ""),  # месяц 1 цифра
+        ("24-03-11", ""),    # год не 4 цифры
+        ("2024-3-11", ""),   # месяц 1 цифра
         ("", ""),
         ("Некорректная дата", ""),
     ],
